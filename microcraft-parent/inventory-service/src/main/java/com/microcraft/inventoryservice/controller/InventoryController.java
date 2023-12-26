95b01afb96b7d098b65b7396ff816d01759c5def
kamlesh <pkamleshkumar12@gmail.com>
2023-12-26 10:35:21 +0800

    REFACTORING all project under microcraft-market

---
+++ microcraft-parent/inventory-service/src/main/java/com/microcraft/inventoryservice/controller/InventoryController.java
@@ -0,0 +1,23 @@
+package com.microcraft.inventoryservice.controller;
+
+
+import com.microcraft.inventoryservice.service.InventoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/inventories")
+@RequiredArgsConstructor
+public class InventoryController {
+
+
+    private final InventoryService inventoryService;
+
+    @GetMapping("/{sku-code}")
+    @ResponseStatus(HttpStatus.OK)
+    public boolean isInStock(@PathVariable("sku-code") String skuCode){
+
+        return inventoryService.isInStock(skuCode);
+    }
+}
---
+++ microcraft-parent/product-service/src/main/java/com/microcraft/productservice/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.microcraft.productservice.repository;
+
+import com.microcraft.productservice.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+
+public interface ProductRepository extends MongoRepository<Product, String> {
+
+}
